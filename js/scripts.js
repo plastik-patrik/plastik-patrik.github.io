17094e12ca8a4cc4e53f95c08dbdf57f0e4218e0
plastik-patrik <72090690+plastik-patrik@users.noreply.github.com>
2024-04-14 00:43:19 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -14,33 +14,13 @@
      * Note: all requests to backend are disabled in this demo, you should use your own backend
      */
 
-    const DemoApp = {
-        initData      : Telegram.WebApp.initData || '',
-        initDataUnsafe: Telegram.WebApp.initDataUnsafe || {},
-        MainButton    : Telegram.WebApp.MainButton,
+    
+    Telegram.WebApp.ready();
+    Telegram.WebApp.MainButton.setParams({
+        text      : 'Замовити',
+        is_visible: true
+    }).onClick(showAlert("Наш менеджер зателефонує Вам для підтвердження замовлення найближчим часом.);
 
-        init(options) {
-            document.body.style.visibility = '';
-            Telegram.WebApp.ready();
-            Telegram.WebApp.MainButton.setParams({
-                text      : 'CLOSE WEBVIEW',
-                is_visible: true
-            }).onClick(DemoApp.close);
-        },
-        expand() {
-            Telegram.WebApp.expand();
-        },
-        close() {
-            Telegram.WebApp.close();
-        },
-        toggleMainButton(el) {
-            const mainButton = Telegram.WebApp.MainButton;
-            if (mainButton.isVisible) {
-                mainButton.hide();
-                el.innerHTML = 'Show Main Button';
-            } else {
-                mainButton.show();
-                el.innerHTML = 'Hide Main Button';
-            }
-        }
-}
+    function showAlert(message) {
+        Telegram.WebApp.showAlert(message);
+    }
